f641516d543c221ac9693a2ba385935a267982e7
SirRGB <sirrgb@proton.me>
2026-08-28 13:49:12 +0200

    feat: append weather forecast time
    * also fix rain forecast

--- src/simple_weather_waveshare/layout/weather_layout.py
+++ src/simple_weather_waveshare/layout/weather_layout.py
@@ -1,5 +1,6 @@
 import logging
 from PIL import Image, ImageDraw
+from datetime import datetime
 from timeit import default_timer as timer
 
 from layout.display_layout_interface import DisplayInterface
@@ -11,18 +12,21 @@ logging.basicConfig(filename='debug.log', format='%(asctime)s %(message)s', leve
 
 
 class WeatherLayout(DisplayInterface):
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+
     def get_display_output(self) -> Image:
-        time = Image.new(mode='RGB', size=(self.screen_length, int(self.screen_height / 2)), color=(255, 255, 255))
+        time = Image.new(mode='RGB', size=(self.screen_length, int(self.screen_height / 2)), color=self.white)
         d1 = ImageDraw.Draw(time)
 
         # draw date and clock
         date_text = f"{get_weekday()}, {get_date()}"
         d1.multiline_text(xy=(self.screen_length / 2, self.screen_height / 9), text=date_text,
-                          font=self.get_font(55), fill=(0, 0, 0), anchor="mm")
+                          font=self.get_font(55), fill=self.black, anchor="mm")
 
         clock_text = f"{get_clock()}"
         d1.multiline_text(xy=(self.screen_length / 2, self.screen_height / 2.5), text=clock_text,
-                          font=self.get_font(95), fill=(0, 0, 0), anchor="mm")
+                          font=self.get_font(95), fill=self.black, anchor="mm")
 
         self.out.paste(time, (0, 0))
 
@@ -32,17 +36,24 @@ class WeatherLayout(DisplayInterface):
         logger.info(f"Fetched data in {elapsed_fetch_time:.3f}")
 
         weather = Image.new(mode='RGB', size=(int(self.screen_length), int(self.screen_height / 2)),
-                            color=(255, 255, 255))
+                            color=self.white)
         d2 = ImageDraw.Draw(weather)
 
-        legend = 'C°\nmm'
+        legend = '\nC°\nmm'
         d2.multiline_text(xy=(int(self.screen_length / 8), int(self.screen_height / 4)), text=legend,
-                          font=self.get_font(35), fill=(0, 0, 0), anchor="mm")
+                          font=self.get_font(35), fill=self.black, anchor="mm")
+
+        time_weather = ['Now']
+        for i in range(1, len(hourly_temp) + 1):
+            clock = datetime.now().hour + i
+            if clock >= 24:
+                clock = clock - 24
+            time_weather.append(str(clock))
 
         for i in range(len(hourly_temp)):
-            weather_text = f'{hourly_temp[i]}\n{hourly_rain[0]}'
+            weather_text = f'{time_weather[i]}\n{hourly_temp[i]}\n{hourly_rain[i]}'
             d2.multiline_text(xy=(int(self.screen_length / 8 * (i + 2)), int(self.screen_height / 4)),
-                              text=weather_text, font=self.get_font(35), fill=(0, 0, 0), anchor="mm")
+                              text=weather_text, font=self.get_font(35), fill=self.black, anchor="mm")
 
         self.out.paste(weather, (0, int(self.screen_height / 2)))
 
