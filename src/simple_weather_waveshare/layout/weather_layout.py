2780942d303484599d0c2d0421fdc3290e8d8ae9
SirRGB <sirrgb@proton.me>
2026-08-28 12:55:24 +0200

    fix: properly align temperature/rain

--- src/simple_weather_waveshare/layout/weather_layout.py
+++ src/simple_weather_waveshare/layout/weather_layout.py
@@ -1,5 +1,5 @@
 import logging
-from PIL.Image import Image
+from PIL import Image, ImageDraw
 from timeit import default_timer as timer
 
 from layout.display_layout_interface import DisplayInterface
@@ -11,31 +11,35 @@ logging.basicConfig(filename='debug.log', format='%(asctime)s %(message)s', leve
 
 class WeatherLayout(DisplayInterface):
     def get_display_output(self) -> Image:
+        time = Image.new(mode='RGB', size=(self.screen_length, int(self.screen_height / 2)), color=(255, 255, 255))
+        d1 = ImageDraw.Draw(time)
+
         # draw date and clock
         date_text = f"{get_weekday()}, {get_date()}"
-        self.d.multiline_text(xy=(self.screen_length / 2, self.screen_height / 4), text=date_text,
-                              font=self.get_font(55), fill=(0, 0, 0), anchor="mm")
-
+        d1.multiline_text(xy=(self.screen_length / 2, self.screen_height / 9), text=date_text,
+                          font=self.get_font(55), fill=(0, 0, 0), anchor="mm")
 
         clock_text = f"{get_clock()}"
-        self.d.multiline_text(xy=(self.screen_length / 2, self.screen_height / 2), text=clock_text,
+        d1.multiline_text(xy=(self.screen_length / 2, self.screen_height / 2.5), text=clock_text,
                               font=self.get_font(95), fill=(0, 0, 0), anchor="mm")
 
+        self.out.paste(time, (0, 0))
 
         start_fetch_time = timer()
         weather_data = get_weather_data()
         elapsed_fetch_time = timer() - start_fetch_time
-        logger.info(f"Rendered in {elapsed_fetch_time:.3f}")
+        logger.info(f"Fetched data in {elapsed_fetch_time:.3f}")
 
-        weather_text = ""
+        weather = Image.new(mode='RGB', size=(int(self.screen_length), int(self.screen_height / 2)), color=(255, 255, 255))
+        d2 = ImageDraw.Draw(weather)
+
+        legend = 'C°\nmm'
+        d2.multiline_text(xy=(int(self.screen_length / 8), int(self.screen_height / 4)), text=legend, font=self.get_font(35), fill=(0, 0, 0), anchor="mm")
+
         for i in range(len(weather_data[0])):
-            weather_text += f"{weather_data[0][i]:^8}"
+            weather_text = f'{weather_data[0][i]}\n{weather_data[1][0]}'
+            d2.multiline_text(xy=(int(self.screen_length / 8 * (i + 2)), int(self.screen_height / 4)), text=weather_text, font=self.get_font(35), fill=(0, 0, 0), anchor="mm")
 
-        weather_text += "\n"
-        for i in range(len(weather_data[1])):
-            weather_text += f"{weather_data[1][i]:^8}"
-
-        self.d.multiline_text(xy=(self.screen_length / 2, self.screen_height / 2 + self.screen_height / 4),
-                              text=weather_text, font=self.get_font(35), fill=(0, 0, 0), anchor="mm")
+        self.out.paste(weather, (0, int(self.screen_height / 2)))
 
         return self.out
